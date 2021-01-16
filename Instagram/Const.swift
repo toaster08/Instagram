f51c4948679f78d2f30c8310e649fdda9e2de1b9
toaster08 <y10star.hon@gmail.com>
2021-01-17 01:36:28 +0900

    Thread 1: Exception: "[<NSObject 0x6000029fc110> setValue:forUndefinedKey:]: this class is not key value coding-compliant for the key captionLabel." 上記修正

---
+++ Instagram/Const.swift
@@ -0,0 +1,16 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 山田　天星 on 2021/01/14.
+//  Copyright © 2021 toaster. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
+
+
